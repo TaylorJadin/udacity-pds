11d73698e42fd15fbfa4660ad6a88516be78552a
Taylor Jadin <jadin.taylor@gmail.com>
2019-03-16 10:57:15 -0500

    notes

--- lesson 2 - joins.sql
+++ lesson 2 - joins.sql
@@ -61,10 +61,29 @@ FROM tablename AS t1
 JOIN tablename2 AS t2
 
 -- EXs
-SELECT t2.primary_poc primary_poc,
-         t1.occurred_at occurred_at,
-         t1.channel channel
-FROM web_events t1
-JOIN accounts t2
-    ON t1.account_id = t2.id
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+ORDER BY a.name;
+
+SELECT r.name region, a.name account, 
+       o.total_amt_usd/(o.total + 0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id;
+
+-- ON statements should always occur with the foreign key being equal to the primary key, though there edge cases where this isn't true
 

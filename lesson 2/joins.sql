4855049782554439e6f735da543268043272f441
Taylor Jadin <jadin.taylor@gmail.com>
2019-03-18 19:57:19 +0000

    notes

--- lesson 2/joins.sql
+++ lesson 2/joins.sql
@@ -98,19 +98,20 @@ ON o.account_id = a.id;
 
 -- last EXs
 
-SELECT r.name RegionName,
-         s.name RepName,
-         a.name AccountName
+SELECT r.name region,
+         s.name rep,
+         a.name account
 FROM region r
 JOIN sales_reps s
     ON r.id = s.region_id
 JOIN accounts a
     ON s.id = a.sales_rep_id
-WHERE r.name like 'Midwest';
+WHERE r.name = 'Midwest'
+ORDER BY a.name;
 
-SELECT r.name RegionName,
-         s.name RepName,
-         a.name AccountName
+SELECT r.name region,
+         s.name rep,
+         a.name account
 FROM region r
 JOIN sales_reps s
     ON r.id = s.region_id
@@ -120,9 +121,9 @@ WHERE r.name LIKE 'Midwest'
 AND s.name LIKE 'S%'
 ORDER BY  a.name;
 
-SELECT r.name RegionName,
-         s.name RepName,
-         a.name AccountName
+SELECT r.name region,
+         s.name rep,
+         a.name account
 FROM region r
 JOIN sales_reps s
     ON r.id = s.region_id
@@ -131,3 +132,43 @@ JOIN accounts a
 WHERE r.name LIKE 'Midwest'
 AND s.name LIKE '% K%'
 ORDER BY  a.name;
+
+SELECT r.name region,
+         a.name rep,
+         o.total_amt_usd/(o.total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+    ON r.id = s.region_id
+JOIN accounts a
+    ON s.id = a.sales_rep_id
+JOIN orders o
+    ON a.id = o.account_id
+WHERE o.standard_qty > 100;
+
+SELECT r.name region,
+         a.name account,
+         o.total_amt_usd/(o.total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+    ON r.id = s.region_id
+JOIN accounts a
+    ON s.id = a.sales_rep_id
+JOIN orders o
+    ON a.id = o.account_id
+WHERE o.standard_qty > 100
+        AND o.poster_qty > 50
+ORDER BY unit_price;
+
+SELECT r.name region,
+         a.name account,
+         o.total_amt_usd/(o.total+0.01) unit_price
+FROM region r
+JOIN sales_reps s
+    ON r.id = s.region_id
+JOIN accounts a
+    ON s.id = a.sales_rep_id
+JOIN orders o
+    ON a.id = o.account_id
+WHERE o.standard_qty > 100
+        AND o.poster_qty > 50
+ORDER BY unit_price DESC;

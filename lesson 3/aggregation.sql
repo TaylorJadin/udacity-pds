5fa1899c7c894140a085be0c6fa671228ddc271f
Taylor Jadin <jadin.taylor@gmail.com>
2019-03-21 02:19:45 +0000

    notes

--- lesson 3/aggregation.sql
+++ lesson 3/aggregation.sql
@@ -40,3 +40,31 @@ FROM orders
 
 SELECT sum(standard_amt_usd)/sum(standard_qty) standard_price_per_unit
 FROM orders
+
+-- MIN and MAX, can be used on non-numerical columns, would return earliest letter in alphabet, earliest date or lowest number
+
+-- AVG, average, mean of the data, does not count NULLs, if you wanted to count NULLs just SUM/COUNT
+
+-- EXs
+SELECT MIN(occurred_at)
+FROM orders
+
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1
+
+SELECT MAX(occurred_at)
+FROM web_events
+
+SELECT occurred_at
+FROM web_events
+ORDER BY  occurred_at DESC limit 1
+
+SELECT avg(standard_qty) avg_standard_qty,
+         avg(gloss_qty) avg_gloss_qty,
+         avg(poster_qty) avg_poster_qty,
+         avg(standard_amt_usd) avg_standard_amt,
+         avg(gloss_amt_usd) avg_gloss_amt,
+         avg(poster_amt_usd) avg_poster_amt
+FROM orders

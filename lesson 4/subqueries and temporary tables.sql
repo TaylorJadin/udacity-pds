3c24bd6359d73a55b0f798de59080ef20d2e7b52
Taylor Jadin <jadin.taylor@gmail.com>
2019-03-25 13:01:16 -0500

    notes

--- lesson 4/subqueries and temporary tables.sql
+++ lesson 4/subqueries and temporary tables.sql
@@ -1,1 +1,12 @@
 -- Subqueries, or inner queries, or nested queries
+SELECT channel,
+	AVG(event_count) AS avg_event_count
+FROM (
+	SELECT DATE_TRUNC('day', occurred_at) AS day,
+		channel,
+		COUNT(*) AS event_count
+	FROM web_events
+	GROUP BY 1,	2
+	) sub
+GROUP BY 1
+ORDER BY 2 DESC;

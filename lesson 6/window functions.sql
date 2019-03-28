c3093ea84f611e565233dde0bb7a6430c22762ad
Taylor Jadin <jadin.taylor@gmail.com>
2019-03-28 16:56:29 -0500

    notes

--- lesson 6/window functions.sql
+++ lesson 6/window functions.sql
@@ -1,5 +1,15 @@
 -- running total using a window function
-SELECT standard_qty SUM(standard_qty) OVER (
+SELECT standard_qty,
+	SUM(standard_qty) OVER (
+		ORDER BY occurred_at
+		) AS running_total
+FROM orders
+
+-- start the total over every month
+SELECT standard_qty,
+    DATE_TRUNC('month',occurred_at) AS month,
+	SUM(standard_qty) OVER (
+        PARTITION BY DATE_TRUNC('month',occurred_at)
 		ORDER BY occurred_at
 		) AS running_total
 FROM orders

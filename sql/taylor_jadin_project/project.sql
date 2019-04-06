09daf8b6d0e71601944d2c2024fb375f56f1beaa
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-06 17:48:31 -0500

    project

---
+++ sql/taylor_jadin_project/project.sql
@@ -0,0 +1,19 @@
+WITH t1
+AS (
+	SELECT f.title film_title,
+		c.name category_name,
+		count(*) rental_count
+	FROM film f
+	JOIN film_category fc ON f.film_id = fc.film_id
+	JOIN category c ON fc.category_id = c.category_id
+	JOIN inventory i ON f.film_id = i.film_id
+	JOIN rental r ON i.inventory_id = r.inventory_id
+	WHERE c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+	GROUP BY 1,	2
+	)
+SELECT category_name,
+	sum(rental_count)
+FROM t1
+GROUP BY 1
+ORDER BY 2 DESC;
+

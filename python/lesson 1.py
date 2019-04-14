34146930da73b3a68697579508849e20e6640b4f
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-14 14:11:22 -0500

    notes

--- python/lesson 1.py
+++ python/lesson 1.py
@@ -6,3 +6,14 @@ print(3 + 5)
 # % Mod (the remainder after dividing)
 # ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
 # // Divides and rounds down to the nearest integer
+
+# ex
+# Write an expression that calculates the average of 23, 32 and 64.
+# Place the expression in this print statement.
+print((23 + 32 + 64)/3)
+
+# Fill this in with an expression that calculates how many tiles are needed.
+print(9*7 +5*7)
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((17*6)-(9*7 + 5*7))

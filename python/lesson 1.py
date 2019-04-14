61dd8f0e3cbb531eceec22a29424136bea850746
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-14 14:03:09 -0500

    ntoes

---
+++ python/lesson 1.py
@@ -0,0 +1,8 @@
+print(3 + 5)
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# % Mod (the remainder after dividing)
+# ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
+# // Divides and rounds down to the nearest integer

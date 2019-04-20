1379d0b640319d7943efb2532f6465be80b6689a
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-20 23:09:05 +0000

    notes

--- python/lesson 1.py
+++ python/lesson 1.py
@@ -45,3 +45,82 @@ reservoir_volume -= 2.5e5
 # print the new value of the reservoir_volume variable
 print(reservoir_volume)
 
+# boolean - comparison operators
+# < > <= >= == =!
+
+# boolean - logican operators
+# and = evaluates if all statements are true
+# or = evaluates if at least one state is true
+# not = not flips the bool value
+
+sf_population, sf_area = 864816, 231.89
+rio_population, rio_area = 6453682, 486.5
+
+san_francisco_pop_density = sf_population/sf_area
+rio_de_janeiro_pop_density = rio_population/rio_area
+
+# Write code that prints True if San Francisco is denser than Rio, and False otherwise
+print (san_francisco_pop_density > rio_de_janeiro_pop_density)
+
+# strings, "" and '' both work except for some edge cases
+# \ escape character
+# you can use + and * to combine and repeat strings
+
+# len, returns length of an object
+
+# TODO: Fix this string!
+ford_quote = "Whether you think you can, or you think you can't--you're right."
+
+#########################
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print( username + " accessed the site " + url + " at " + timestamp + ".")
+
+#########################
+
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+full_name = given_name + " " + middle_names + " " + family_name
+name_length = len(full_name)
+
+# Now we check to make sure that the name fits within the driving license character limit
+# Nothing you need to do here
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
+
+# type conversion
+>>> print(type(4))
+int
+>>> print(type(3.7))
+float
+>>> print(type('this'))
+str
+>>> print(type(True))
+bool
+
+################################
+
+weekly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+weekly_sales = str(weekly_sales)  #convert the type back!!
+print("This week's total sales: " + weekly_sales)
+
+# Methods, functions that belong to an object
+# Methods have arguments that go in (), although the first argument is the object 
+
+# format
+print("Mohammed has {} balloons".format(27))
+# Mohammed has 27 balloons
+animal = "dog"
+action = "bite"
+print("Does your {} {}?".format(animal, action))
+# Does your dog bite?
+maria_string = "Maria loves {} and {}"
+print(maria_string.format("math", "statistics"))
+# Maria loves math and statistics

a4f734f3a3fc9509f2b937746a33033bfc986704
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-20 22:28:10 +0000

    notes

--- python/lesson 1.py
+++ python/lesson 1.py
@@ -17,3 +17,31 @@ print(9*7 +5*7)
 
 # Fill this in with an expression that calculates how many tiles will be left over.
 print((17*6)-(9*7 + 5*7))
+
+# quiz with variables
+
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
+
+# decrease the rainfall variable by 10% to account for runoff
+rainfall *= .9
+
+# add the rainfall variable to the reservoir_volume variable
+reservoir_volume += rainfall
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+# into the reservoir in the days following the storm
+reservoir_volume *= 1.05
+
+# decrease reservoir_volume by 5% to account for evaporation
+reservoir_volume *= .95
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+reservoir_volume -= 2.5e5
+
+# print the new value of the reservoir_volume variable
+print(reservoir_volume)
+

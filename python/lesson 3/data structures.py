718f9c49d179ac03be2498a9b5892d41711264cd
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-28 16:38:06 -0500

    notes

--- python/lesson 3/data structures.py
+++ python/lesson 3/data structures.py
@@ -112,3 +112,22 @@ print("grades: " + str(grades))
 # max() returns the greatest element of the list. How the greatest element is determined depends on what type objects are in the list. The maximum element in a list of numbers is the largest number. The maximum elements in a list of strings is element that would occur last if the list were sorted alphabetically. This works because the the max function is defined in terms of the greater than comparison operator. The max function is undefined for lists that contain elements from different, incomparable types.
 # min() returns the smallest element in a list. min is the opposite of max, which returns the largest element in a list.
 # sorted() returns a copy of a list in order from smallest to largest, leaving the list unchanged.
+
+# join, takes a list of strings as an arg and returns one string with the elements joined by a separator string
+new_str = "\n".join(["fore", "aft", "starboard", "port"])
+print(new_str)
+# output: fore
+# output: aft
+# output: starboard
+# output: port
+
+name = "-".join(["García", "O'Kelly"])
+print(name)
+# output: Garcia-O'Kelly
+
+# append, adds an element to the end of a list
+letters = ['a', 'b', 'c', 'd']
+letters.append('z')
+print(letters)
+# output: ['a', 'b', 'c', 'd', 'z']
+

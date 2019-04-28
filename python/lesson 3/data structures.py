f358071829c11eb4d8c16de6f8354f8e57feb0a3
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-28 16:52:32 -0500

    notes

--- python/lesson 3/data structures.py
+++ python/lesson 3/data structures.py
@@ -138,4 +138,14 @@ c = [100, 200]
 
 print(max([len(a), len(b), len(c)]))
 print(min([len(a), len(b), len(c)]))
+# output: 4,1
+#############################################
+names = ["Carol", "Albert", "Ben", "Donna"]
+print(" & ".join(sorted(names)))
+# output: Albert & Ben & Carol & Donna
+#############################################
+names = ["Carol", "Albert", "Ben", "Donna"]
+names.append("Eugenia")
+print(sorted(names))
+# output: ['Albert', 'Ben', 'Carol', 'Donna', 'Eugenia']
 #############################################

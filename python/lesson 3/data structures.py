d6180e2c90e36e20bcad8581821d38facbda96ef
Taylor Jadin <jadin.taylor@gmail.com>
2019-04-28 14:42:51 -0500

    notes

--- python/lesson 3/data structures.py
+++ python/lesson 3/data structures.py
@@ -107,3 +107,8 @@ print("scores: " + str(scores))
 print("grades: " + str(grades))
 # output: scores: ['B', 'C', 'A', 'B', 'B', 'A']
 
+# useful functions for lists:
+# len() returns how many elements are in a list.
+# max() returns the greatest element of the list. How the greatest element is determined depends on what type objects are in the list. The maximum element in a list of numbers is the largest number. The maximum elements in a list of strings is element that would occur last if the list were sorted alphabetically. This works because the the max function is defined in terms of the greater than comparison operator. The max function is undefined for lists that contain elements from different, incomparable types.
+# min() returns the smallest element in a list. min is the opposite of max, which returns the largest element in a list.
+# sorted() returns a copy of a list in order from smallest to largest, leaving the list unchanged.

9325f5e8a7abf5f5810044c88145d4ca1a52d01b
Taylor Jadin <jadin.taylor@gmail.com>
2019-05-06 00:37:10 -0500

    notes'

--- python/lesson 6/scripting.py
+++ python/lesson 6/scripting.py
@@ -14,3 +14,23 @@ Welcome to Python3!
 
 
 print(snake_string * how_many_snakes)
+
+# scripting with raw input
+name = input("Enter your name: ")
+print("Hello there, {}!".format(name.title()))
+
+# wrapping with int
+num = int(input("Enter an integer"))
+print("hello" * num)
+
+# wrap with float to catch non integers
+num = float(input("Enter an number"))
+print("hello" * num)
+
+# or wrap with both to catch a non integer and convert back to int
+num = int(float(input("Enter an number")))
+print("hello" * num)
+
+# allow users to input a python expression with eval
+result = eval(input("Enter an expression: "))
+print(result)

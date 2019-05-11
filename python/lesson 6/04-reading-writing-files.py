dc2bff9afd2cc56718b149b8002c041bbc81e30f
Taylor Jadin <jadin.taylor@gmail.com>
2019-05-11 14:45:43 +0000

    notes

--- python/lesson 6/04-reading-writing-files.py
+++ python/lesson 6/04-reading-writing-files.py
@@ -7,6 +7,21 @@ f.close()
 print (file_data)
 
 # opening a file in writing mode
-f = open('some_file.txt', 'w')
+f = open('my_path/my_file.txt', 'w')
+f.write("Hello there!")
+f.close()
 # this will actually erase the contents of the file, if you want to add to a file use 'a' for append
 
+# too many open files
+files = []
+for i in range(10000):
+    files.append(open('some_file.txt', 'r'))
+    print(i)
+
+# with
+# this is a special syntax that opens the file and performs actions and closes it automatically
+
+with open('my_path/my_file.txt', 'r') as f:
+    file_data = f.read()
+
+print(file_data)

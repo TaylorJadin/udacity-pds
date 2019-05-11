1d936cee6b64f831e225b58e2ad8262a0aa0345c
Taylor Jadin <jadin.taylor@gmail.com>
2019-05-11 15:20:32 +0000

    notes

--- python/lesson 6/04-reading-writing-files.py
+++ python/lesson 6/04-reading-writing-files.py
@@ -25,3 +25,34 @@ with open('my_path/my_file.txt', 'r') as f:
     file_data = f.read()
 
 print(file_data)
+
+# you can pass the read method an integer and it will read up to that character, then leave the window there to resume later:
+with open("camelot.txt") as song:
+    print(song.read(2))
+    print(song.read(8))
+    print(song.read())
+# output:
+# We
+# 're the 
+# knights of the round table
+# We dance whenever we're able
+
+# readline will read one line of a file at a time instead of outputting a '\n'
+
+f.readline()
+# output: 'This is the first line of the file.\n'
+f.readline()
+# output: 'Second line of the file\n'
+f.readline()
+# output: ''
+
+# read in the lines of a file and write them to a list
+camelot_lines = []
+with open("camelot.txt") as f:
+    for line in f: # this allows you to loop through the lines in a file
+        camelot_lines.append(line.strip()) # read lines, but strip the trailing whitespace
+
+print(camelot_lines)
+# output: ["We're the knights of the round table", "We dance whenever we're able"]
+
+
---
+++ python/lesson 6/05-cast_list.py
@@ -0,0 +1,11 @@
+def create_cast_list(filename):
+    cast_list = []
+    #use with to open the file filename
+    #use the for loop syntax to process each line
+    #and add the actor name to cast_list
+
+    return cast_list
+
+cast_list = create_cast_list('flying_circus_cast.txt')
+for actor in cast_list:
+    print(actor)

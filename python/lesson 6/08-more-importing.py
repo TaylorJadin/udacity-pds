af36c6ce79a0f4fa29d992e7545463970ee5f143
Taylor Jadin <jadin.taylor@gmail.com>
2019-05-12 19:30:10 +0000

    notes

---
+++ python/lesson 6/08-more-importing.py
@@ -0,0 +1,8 @@
+from module_name import object_name
+from module_name import first_object, second_object
+import module_name as new_name
+from module_name import object_name as new_name
+#never do this:
+from module_name import *
+# import submodule from a package
+import package_name.submodule_name
